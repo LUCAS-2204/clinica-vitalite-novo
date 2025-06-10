dd1ded59bb0dbd8c23d8ff47900a691db896ea1e
LUCAS-2204 <lucas.cmaciel@outlook.com>
2025-06-10 17:52:10 -0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,19 @@
 
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "SUA-CHAVE-AQUI",
-  authDomain: "SEU-PROJETO.firebaseapp.com",
-  projectId: "SEU-ID",
-  storageBucket: "SEU-PROJETO.appspot.com",
-  messagingSenderId: "ID-AQUI",
-  appId: "APP-ID-AQUI"
+   apiKey: "AIzaSyBWYeVM087npG2KHi8PFOv33A0hQsi5ppk",
+  authDomain: "clinica-vitalite.firebaseapp.com",
+  projectId: "clinica-vitalite",
+  storageBucket: "clinica-vitalite.firebasestorage.app",
+  messagingSenderId: "458561544439",
+  appId: "1:458561544439:web:6321568b90e3571ee733f2"
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const auth = getAuth(app);
 
-export { db };
+export { db, auth };
